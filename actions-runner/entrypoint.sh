2d9c29c46c8b5e3988101f6974b7f9a4eec6ef29
christianmz565 <christianmz565@gmail.com>
2026-06-07 23:10:08 -0500

    chore: modify image to work via sidecar dind

--- actions-runner/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting Docker daemon..."
-sudo dockerd --host=unix:///var/run/docker.sock > /dev/null 2>&1 &
-timeout 15 bash -c 'until sudo docker info >/dev/null 2>&1; do sleep 1; done'
-
-echo "Starting github runner..."
-exec /home/runner/run.sh "$@"
